d57882559a0006c535a19b8e11104a54dccae691
Mohammed Abdul Raheem <raheemnadeem270@gmail.com>
2023-04-04 14:44:36 -0500

    Added routes.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import Login from './Components/Login/Login'
 import Register from './Components/Register/Register'
 import {
   createBrowserRouter,
+  Navigate,
   RouterProvider,
 } from "react-router-dom";
+import Home from './Components/Home/Home';
+
+const currentUser = false;
+
+const ProtectedRoute = ({children})=>{
+  if(!currentUser){
+    return <Navigate to="/Login"/>
+  } else return children
+}
 
 const router = createBrowserRouter([
+  {path:"/",
+  element: currentUser ? <Home/> : <Login/>
+  },
   {
     path: "/Login",
     element: <Login/>,
@@ -14,8 +27,14 @@ const router = createBrowserRouter([
   {
     path:"/Register",
     element:<Register/>
+  },
+  {
+    path:"/Home",
+    element: <ProtectedRoute><Home/></ProtectedRoute>
   }
 ]);
+
+
 
 
 export default function App() {
